94d72c450d2ec8ce8f1699af694715fb537f5632
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-15 19:17:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,6 +25,50 @@ app.get('/api/student/:id',(req,res)=>{
     res.send(student);
 });
 
+app.post('/api/student',(req,res)=>{
+    const name=req.body.name;
+    const currentClass=req.body.currentClass;
+    const division=req.body.division;
+
+    if(name && currentClass && division){
+        students.push({id:students.length+1,name,currentClass,division});
+        res.send(students);
+    }else{
+        res.status(400).send("invalid request");
+    }
+});
+
+app.put('/api/student/:id',(req,res)=>{
+    const id=req.params.id;
+    const findStudent=students.find((stu)=>stu.id===parseInt(id));
+    
+    if(findStudent){
+        const student=students.filter((stu)=>stu.id===parseInt(id));
+
+        res.set('Content-Type', 'application/x-www-form-urlencoded');
+        student.name=req.body.name;
+        student.currentClass=parseInt(req.body.currentClass);
+        student.division=req.body.division;
+        res.send(students);
+    }else{
+        res.status(400).send("invalid id not found");
+    }
+});
+
+app.delete('/api/student/:id',(req,res)=>{
+    const id=req.params.id;
+
+    const findStudent=students.find((stu)=>stu.id===parseInt(id));
+
+    if(findStudent){
+        const filteredStudents=students.filter((stu)=>stu.id!==parseInt(id));
+        students=filteredStudents;
+        res.send(students);
+    }else{
+        res.status(400).send('id not found');
+    }
+});
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
