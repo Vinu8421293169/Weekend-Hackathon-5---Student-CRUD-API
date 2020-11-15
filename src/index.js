17cfca9dc78d6a8f789a7abd8a7c6835a847b41e
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-15 18:01:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-const express = require('express')
-const app = express()
-const bodyParser = require("body-parser");
-const port = 8080
-app.use(express.urlencoded());
+const express=require('express');
+const students=require('./InitialData.js');
 
-// Parse JSON bodies (as sent by API clients)
+const app=express();
+
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-const students=require('InitialData.js');
 
 app.get('/api/student',()=>{
-   res.send(students);
+   res.send(students); 
 });
 
 
@@ -24,36 +19,34 @@ app.get('/api/student/:id',(req,res)=>{
     res.send(student);
 });
 
-app.post('/api/student',(req,res)=>{
-    const name=req.body.name;
-    const currentClass=req.body.currentClass;
-    const division=req.body.division;
-    if(!name || !currentClass || !division){
-        res.status(400).send("invalid request");
-    }else{
-        students.push({id=students.length+1,...body});
-    }
-});
+// app.post('/api/student',(req,res)=>{
+//     const name=req.body.name;
+//     const currentClass=req.body.currentClass;
+//     const division=req.body.division;
+//     if(!name || !currentClass || !division){
+//         res.status(400).send("invalid request");
+//     }else{
+//         students.push({id=students.length+1,...body});
+//     }
+// });
 
-app.put('/api/student',(req,res)=>{
-    res.set('Content-Type', 'application/x-www-form-urlencoded');
-})
+// app.put('/api/student',(req,res)=>{
+//     res.set('Content-Type', 'application/x-www-form-urlencoded');
+// })
 
-app.delete('/api/student',(req,res)=>{
-    const id=req.body.id;
+// app.delete('/api/student',(req,res)=>{
+//     const id=req.body.id;
 
-    const findStudent=students.find((stu)=>{
-        stu.id===id;
-    });
-    if(!findStudent){
-        res.status(400)
-    }else{
-        const filteredStudents=students.filter((stu)=>{
-            stu.id!==id;
-        })
-    }
-})
+//     const findStudent=students.find((stu)=>{
+//         stu.id===id;
+//     });
+//     if(!findStudent){
+//         res.status(400)
+//     }else{
+//         const filteredStudents=students.filter((stu)=>{
+//             stu.id!==id;
+//         })
+//     }
+// })
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
-
-module.exports = app;
+// // console.log(students);
